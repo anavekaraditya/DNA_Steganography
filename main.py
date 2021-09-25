baad38d8c354c6f028c91b5d9dbd429b91dfd436
anavekaraditya <anavekar.aditya@gmail.com>
2021-09-26 02:09:52 +0530

    amino acids added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def m_to_mbin(message):
     for i in message:
         m_ascii = ord(i)
         m_ascii_bin = bin(m_ascii)[0] + bin(m_ascii)[2:]
-        mbin = mbin + str(m_ascii_bin)
+        mbin += str(m_ascii_bin)
     return mbin
 
 mbin = m_to_mbin(message)
@@ -16,14 +16,23 @@ def mbin_to_mdna(mbin):
     for i in range(0, len(mbin), 2):
         j = mbin[i:i+2]
         if j == "00" :
-            mdna = mdna + 'A'
+            mdna += 'A'
         elif j == "01":
-            mdna = mdna + 'C'
+            mdna += 'C'
         elif j == "10":
-            mdna = mdna + 'G'
+            mdna += 'G'
         elif j == "11":
-            mdna = mdna + 'T'
+            mdna += 'T'
     return mdna
     
 mdna = mbin_to_mdna(mbin)
+mdna = mdna.replace('T','U')
 print(mdna)
+
+amino_acids = [[['GCU','GCC','GCA','GCG'],['A']],[['CGA','CGG','AGA','AGG'],['R']],[['AAU','AAC'],['N']],
+               [['GAU','GAC'],['D']],[['UGU','UGC'],['C']],[['CAA','CAG'],['Q']],[['GAA','GAG'],['E']],
+               [['GGU','GGC','GGA','GGG'],['G']],[['CAU','CAC'],['H']],[['AUU','AUC','AUA'],['I']],
+               [['UAA','UGA','UAG'],['B']],[['CGU','CGC'],['U']],[['GUC'],['Z']],[['UUA','UUG','CUU'],['L']],
+               [['AAA','AAG'],['K']],[['AUG'],['M']],[['UUC','UUU'],['F']],[['CCU','CCC','CCA'],['P']],
+               [['UCU','UCC','AGU','AGC'],['S']],[['ACU','ACC','ACA','ACG'],['T']],[['UGG'],['W']],
+               [['UAU','UAC'],['Y']],[['GUU','GUA'],['V']],[['CUA','CUG'],['O']],[['UCA','UCG'],['X']],[['CUC','CCG','GUG'],['J']]]
